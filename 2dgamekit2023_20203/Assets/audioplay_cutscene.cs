8818b467ac6d95c7e0306a7b56672bb1232b1f16
KodiakJc <130916301+KodiakJc@users.noreply.github.com>
2023-04-16 17:11:58 +0200

    Project base

---
+++ 2dgamekit2023_20203/Assets/audioplay_cutscene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioplay_cutscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemies/Gunner/GunnerIntro", transform.position);
+    }
+
+}
---
+++ 2dgamekit2023_20203/Assets/audioplay_enemies.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioplay_enemies : MonoBehaviour
+{
+    void StartAttack(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+    }
+
+    void StartDeath(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+    }
+
+    void PlayFootStep(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+    }
+
+    void Shooting(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+    }
+
+    //gunner
+
+    void PlayStep(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+    }
+
+}
